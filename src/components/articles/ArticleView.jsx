99f2c6b7ad6a38855e36009c11dff4dbf19d90c5
Marie-Louise Preira <mlpreira@gmail.com>
2021-09-28 20:41:33 +0200

    ajout du bouton retour

--- src/components/articles/ArticleView.jsx
+++ src/components/articles/ArticleView.jsx
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from "react";
-import {useParams} from "react-router-dom";
+import {useParams, useHistory} from "react-router-dom";
 import {instance as axios} from "../../config/axiosConfig";
 import Moment from "react-moment";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPaperPlane, faShare} from "@fortawesome/free-solid-svg-icons";
+import {faPaperPlane, faShare, faAngleLeft} from "@fortawesome/free-solid-svg-icons";
 
 //components
 import NotFound from "../404notfound/NotFound";
@@ -11,6 +11,7 @@ import Loader from "../common/Loader";
 
 const ArticleView = ({articles}) => {
 
+    let history = useHistory();
     let { articleId } = useParams();
     const [selected, setSelected] = useState({});
     const [loading, setLoading] = useState(true);
@@ -35,32 +36,41 @@ const ArticleView = ({articles}) => {
     }, [articleId]);
 
     return !loading? (!notFound ?(
-        <div className="body article">
-            <div className="flex flex-col items-center">
-                <img src={selected.image} alt="" />
+        <>
+            <button
+                className="mt-4 ml-4 gold"
+                onClick={() => {history.goBack()}}
+            >
+                <FontAwesomeIcon icon={faAngleLeft} className="mr-2" />
+                Retour
+            </button>
+            <div className="body article">
+                <div className="flex flex-col items-center">
+                    <img src={selected.image} alt="" />
 
-                <h2>{selected.title}</h2>
+                    <h2>{selected.title}</h2>
 
-                <div className="details">
-                    <span>publié par {selected.user_id} le <Moment format="L">{selected.created_at}</Moment></span>
-                    <span>mis à jour le <Moment format="L">{selected.updated_at}</Moment></span>
+                    <div className="details">
+                        <span>publié par {selected.user_id} le <Moment format="L">{selected.created_at}</Moment></span>
+                        <span>mis à jour le <Moment format="L">{selected.updated_at}</Moment></span>
+                    </div>
+
+                    <div className="flex justify-end w-full mt-2 text-gray-400">
+                        <button>
+                            <FontAwesomeIcon icon={faPaperPlane} className="lg:mr-2 mr-5" />
+                        </button>
+                        <button>
+                            <FontAwesomeIcon icon={faShare} className="lg:mr-2 mr-5" />
+                        </button>
+                    </div>
+
+                    <div className="content">
+                        {selected.content}
+                    </div>
+
                 </div>
-
-                <div className="flex justify-end w-full mt-2 text-gray-400">
-                    <button>
-                        <FontAwesomeIcon icon={faPaperPlane} className="lg:mr-2 mr-5" />
-                    </button>
-                    <button>
-                        <FontAwesomeIcon icon={faShare} className="lg:mr-2 mr-5" />
-                    </button>
-                </div>
-
-                <div className="content">
-                    {selected.content}
-                </div>
-
             </div>
-        </div>
+        </>
     ) : <NotFound />) : (<Loader />)
 }
 export default ArticleView;
